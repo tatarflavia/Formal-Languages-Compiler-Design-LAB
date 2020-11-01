c46be03b94229d729b6e9158799226112905bf78
tatarflavia <tatar.flavia25@gmail.com>
2020-11-01 22:12:17 +0200

    ready for code review

---
+++ lab3/lab3-proj/src/Domain/ProgramInternalForm.java
@@ -0,0 +1,26 @@
+package Domain;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+public class ProgramInternalForm {
+    private List<Map.Entry<String,Integer>> pairList=new ArrayList<>();
+
+    public ProgramInternalForm() {
+    }
+
+
+
+
+    public void addInPIF(String token){
+        this.pairList.add(new AbstractMap.SimpleEntry<String,Integer>(token, 0));
+    }
+
+
+}
+*/
