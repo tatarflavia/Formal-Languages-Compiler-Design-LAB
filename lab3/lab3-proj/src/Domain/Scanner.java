56adbc097b0b1e514b461b083d09005d5391d7da
tatarflavia <61745471+tatarflavia@users.noreply.github.com>
2020-11-01 22:14:59 +0200

    Update Scanner.java

--- lab3/lab3-proj/src/Domain/Scanner.java
+++ lab3/lab3-proj/src/Domain/Scanner.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-/*
+
 
 public class Scanner {
     private ProgramInternalForm PIF;
@@ -119,4 +119,4 @@ public class Scanner {
         }
     }
 }
-*/
+
